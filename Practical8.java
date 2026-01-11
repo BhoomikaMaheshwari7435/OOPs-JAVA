1a15910cebc9adf0a2f29323fd53440afbb89bdc
Bhoomi Maheshwari <24becebhoomikamaheshwari@saffrony.ac.in>
2026-01-11 15:11:10 +0530

    Add files via upload

---
+++ Practical8.java
@@ -0,0 +1,15 @@
+public class Practical8{
+	public static void main(String[] args){
+		
+		System.out.println("\nBhoomika Maheshwari");
+		System.out.println("Enrollment No.: 240390107020\n");
+			int a = Integer.parseInt(args[0]);
+			int b = Integer.parseInt(args[1]);
+			
+			int sum = a+b;
+			int mul = a*b;
+			System.out.println("Sum : "+sum);
+			System.out.println("Multiply : "+mul);
+	}
+
+}
