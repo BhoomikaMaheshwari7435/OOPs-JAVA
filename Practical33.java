372f064441c9b97ad9d7067ad2060b969e3f2f77
Bhoomi Maheshwari <24becebhoomikamaheshwari@saffrony.ac.in>
2026-04-25 21:14:43 +0530

    Add files via upload

---
+++ Practical33.java
@@ -0,0 +1,27 @@
+public class Practical33 {
+
+    public static void main(String[] args) {
+		
+		System.out.print("\nBhoomika Maheshwari");
+		System.out.println("\nEnrollment Number: 240390107020\n");
+
+        try {
+
+            int numerator = Integer.parseInt(args[0]);
+            int denominator = Integer.parseInt(args[1]);
+
+            int result = numerator / denominator;
+
+            System.out.println("Result = " + result);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Please provide two arguments");
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid number format");
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        } catch (Exception e) {
+            System.out.println("Some error occurred");
+        }
+    }
+}
